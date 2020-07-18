3e5f7fa3aa89eb02bae790f02141d77682db7e37
Rail <rail1999g102r@mail.ru>
2020-07-18 18:58:51 +0500

    0. Spring MVC [#320392]

--- src/main/java/ru/job4j/di/SpringDI.java
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -3,11 +3,11 @@ package ru.job4j.di;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SpringDI {
-    public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.scan("ru.job4j.di");
-        context.refresh();
-        System.out.println(context.getBean(ConsoleInput.class) == context.getBean(ConsoleInput.class));
-        System.out.println(context.getBean(Store.class) == context.getBean(Store.class));
-    }
+//    public static void main(String[] args) {
+//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+//        context.scan("ru.job4j.di");
+//        context.refresh();
+//        System.out.println(context.getBean(ConsoleInput.class) == context.getBean(ConsoleInput.class));
+//        System.out.println(context.getBean(Store.class) == context.getBean(Store.class));
+//    }
 }
