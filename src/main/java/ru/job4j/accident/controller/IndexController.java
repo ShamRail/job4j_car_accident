7b5f905518f99db71385e5d015d8b0942623a58a
Rail <rail1999g102r@mail.ru>
2020-07-18 19:08:54 +0500

    1. Загрузка данных в вид [#320387]

--- src/main/java/ru/job4j/accident/controller/IndexController.java
+++ src/main/java/ru/job4j/accident/controller/IndexController.java
@@ -4,11 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class IndexController {
 
     @GetMapping("/")
     public String index(Model model) {
+        model.addAttribute("names", List.of(
+                "Ivan",
+                "Boris",
+                "Boris",
+                "Petr",
+                "Michael"
+        ));
         return "index";
     }
 
